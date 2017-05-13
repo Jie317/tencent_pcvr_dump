92633ef4f3191d68fdeda8e1c079e6187b4dec26
Jie317 <jie.he@mines-ales.org>
2017-05-13 11:12:12 +0200

    Update

--- quick_push.sh
+++ quick_push.sh
@@ -15,8 +15,8 @@ git push
 
 # Other tips
 # 1 ssh login: 
-https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/ then 
-run: ssh-keyscan -t rsa 
+# https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/ then 
+# run: ssh-keyscan -t rsa 
 # github.com >> ~/.ssh/known_hosts
 # 2 init a repo: 
 # git init && git remote add origin git@github.com:Jie317/***.git
