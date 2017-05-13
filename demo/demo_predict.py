7263fb78d7953bfff97a8c6c61d30e899ff394d3
zhuyin <775941748@qq.com>
2017-05-13 23:31:55 +0800

    train with generated_data

--- demo/demo_predict.py
+++ demo/demo_predict.py
@@ -17,39 +17,40 @@ from sklearn.externals import joblib
 '''
 导入模型参数
 '''
-model_file = 'D:\dataScience\pre\model-avazu-sgd4.pkl'
+model_file = 'D:\dataScience\pre\model-avazu-sgd6.pkl'
 cls = joblib.load(model_file)
 
-preproc_file = 'D:\dataScience\pre\model-avazu-preproc4.pkl'
+preproc_file = 'D:\dataScience\pre\model-avazu-preproc6.pkl'
 preproc = joblib.load(preproc_file)
 
 '''
 数据清洗
 '''
 
-
 def clean_data(train_data):
-    #    train_data['day'] = train_data['clickTime'].map(lambda x :int(str(x)[0:2]))
-    train_data['hour'] = train_data['clickTime'].map(lambda x: int(str(x)[2:4]))
-    train_data['mins'] = train_data['clickTime'].map(lambda x: int(str(x)[4:6]))
-    x = train_data.drop(['label', 'instanceID', 'clickTime'], axis=1)
+#    train_data['day'] = train_data['clickTime'].map(lambda x :int(str(x)[0:2]))
+    train_data['hour'] = train_data['clickTime'].map(lambda x :int(str(x)[2:4]))
+    train_data['mins'] = train_data['clickTime'].map(lambda x :int(str(x)[4:6]))
+    x = train_data.drop(['label','instanceID','clickTime'],axis = 1)
     x = np.asarray(x.astype(str))
     x = preproc.fit_transform(x)
     return x
 
 
 chunk_size = 10000
-reader = pd.read_csv(r'D:\dataScience\pre\test.csv',
-                     chunksize=chunk_size, )
-# seed = 0  seed尚未定义
+reader = pd.read_csv(r'D:\dataScience\pre\new_generated_test.csv',
+                       chunksize=chunk_size,)
+#seed = 0  seed尚未定义
 i = 0
-with open(r'D:\dataScience\pre\submission5.csv', 'a') as outfile:
+with open(r'D:\dataScience\pre\submission6.csv','a') as outfile:
     outfile.write('instanceID, prob\n')
     for data in reader:
-        i += 1
-        instanceID = data['instanceID'].values
-        x = clean_data(data)
-        prob = cls.predict_proba(x)[:, 1]
-        dfjo = pd.DataFrame(dict(instanceID=instanceID, prob=prob), columns=['instanceID', 'prob'])
+        i+=1
+        instanceID=data['instanceID'].values
+        x=clean_data(data)
+        prob =cls.predict_proba(x)[:,1]
+        dfjo = pd.DataFrame(dict(instanceID=instanceID,prob=prob), columns=['instanceID','prob'])
         dfjo.to_csv(outfile, header=None, index_label=None, index=False)
         print(i)
+
+    
---
+++ demo/generate_test_data.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 13 23:14:50 2017
+
+@author: Z.Y
+"""
+
+import os
+import pandas as pd
+import numpy as np
+from collections import Counter
+from time import time
+
+d = 'D:/dataScience/pre/'
+
+user = pd.read_csv('%suser.csv' % d)
+ad = pd.read_csv('%sad.csv' % d)
+position = pd.read_csv('%sposition.csv' % d)
+
+'''
+为了简单起见，直接把下面的train 改为test
+
+test = pd.read_csv('%stest.csv' % d)
+train = pd.read_csv('%strain.csv' % d)
+'''
+train = pd.read_csv('%stest.csv' % d)
+
+app_categories = pd.read_csv('%sapp_categories.csv' % d)
+
+user_info = user.set_index('userID').ix[train['userID']].reset_index()
+creative_info = ad.set_index('creativeID').ix[train['creativeID']].reset_index()
+position_info = position.set_index('positionID').ix[train['positionID']].reset_index()
+ad_info = pd.concat([creative_info, position_info], axis=1)
+
+action_info = train[['clickTime', 'connectionType', 'telecomsOperator', 'instanceID']]
+user_info = user_info.drop('userID', axis=1)
+ad_info = ad_info.drop(['creativeID', 'positionID'], axis=1)
+
+new_train = pd.concat([user_info, ad_info, action_info, train['label']], axis=1)
+
+new_train.to_csv('%snew_generated_test.csv' % d)
