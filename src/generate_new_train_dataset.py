653a452695a2c65a46b2d4751b53628d61eec29a
Jie317 <jie.he@mines-ales.org>
2017-05-13 16:50:07 +0200

    Enable generating test dataset from raw csv files

--- src/generate_new_train_dataset.py
+++ src/generate_new_train_dataset.py
@@ -35,42 +35,44 @@ s = Counter(train['label'])
 print(s[1]/s[0], s)
 
 
-# ## Parse the train.csv which mainly consists of three feature groups: user info, ad info and action info(click and conversion)
+# ## Parse the train.csv and test csv which mainly consist of three feature groups: user info, ad info and action info(click and conversion)
+
+# In[126]:
+
+generate_train = False # True for train, False for test
+if generate_train:
+    data = train
+else: 
+    data = test
+
+
 # ### 1 User info (user, user_installedapps and user_app_actions)
 
-# In[105]:
+# In[127]:
 
-user_info = user.set_index('userID').ix[train['userID']].reset_index()
+user_info = user.set_index('userID').ix[data['userID']].reset_index()
 
 
 # ### 2 Ad info (creative and position)
 
-# In[106]:
+# In[128]:
 
-creative_info = ad.set_index('creativeID').ix[train['creativeID']].reset_index()
-
-
-# In[107]:
-
-position_info = position.set_index('positionID').ix[train['positionID']].reset_index()
-
-
-# In[108]:
-
+creative_info = ad.set_index('creativeID').ix[data['creativeID']].reset_index()
+position_info = position.set_index('positionID').ix[data['positionID']].reset_index()
 ad_info = pd.concat([creative_info, position_info], axis=1)
 
 
 # ### 3 Action info (clickTime, conversionTime, connectionType, and telecomsOperator)
 
-# In[109]:
+# In[129]:
 
 action_info = train[['clickTime', 'conversionTime','connectionType', 'telecomsOperator']]
 
 
 # ## Generated training dataset
-# ### Drop out the id columns
+# ### Drop out the id columns (or we keep them?)
 
-# In[110]:
+# In[130]:
 
 user_info = user_info.drop('userID', axis=1)
 ad_info = ad_info.drop(['creativeID', 'positionID'], axis=1)
@@ -78,14 +80,19 @@ ad_info = ad_info.drop(['creativeID', 'positionID'], axis=1)
 
 # ### Concatenate the three feature groups, appended by label column
 
-# In[112]:
+# In[131]:
 
-new_train = pd.concat([user_info, ad_info, action_info, train['label']], axis=1)
+if generate_train:
+    new_train = pd.concat([user_info, ad_info, action_info, data['label']], axis=1)
+    new_train.to_csv('%snew_generated_train.csv' % d)
+else:
+    new_test = pd.concat([user_info, ad_info, action_info], axis=1)
+    new_test.to_csv('%snew_generated_test.csv' % d)
 
 
 # In[113]:
 
-new_train.to_csv('%snew_generated_train.csv' % d)
+
 
 
 # In[ ]:
