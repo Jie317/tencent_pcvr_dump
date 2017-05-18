93a2abb64aabf8ceda72761732bb376012ceb8c9
zhuyin <775941748@qq.com>
2017-05-18 08:24:40 +0800

    modify some mistake

--- perpare_data/generate_ffm_raw_test.py
+++ perpare_data/generate_ffm_raw_test.py
@@ -44,23 +44,24 @@ ffm_raw.fillna(0, inplace=True)
 ffm_raw.to_csv('%sformatted_ffm_raw_test4.csv' % d)
 print('ffm_raw_test completed')
 
-from collections import OrderedDict
 
-fields = tuple(list(OrderedDict.fromkeys(i.split('_')[0] for i in ffm_raw.columns)))  # including 'label'
-features = ffm_raw.columns
-
-
-def ffm_format(row):
-    str_ = '1' if int(row[0]) == 1 else '-1'
-    for i, v in enumerate(row[1:]):
-        # get rid of zero values 
-        if v == 0:
-            continue
-        else:
-            str_ += '\t%d:%d:%s' % (fields.index(features[i + 1].split('_')[0]) - 1, i, str(v))
-    return str_
-
-
-formatted = ffm_raw.apply(ffm_format, axis=1)
-
-formatted.to_csv('%sformatted_test2.csv' % d, header=False, index=False)
+# from collections import OrderedDict
+#
+# fields = tuple(list(OrderedDict.fromkeys(i.split('_')[0] for i in ffm_raw.columns)))  # including 'label'
+# features = ffm_raw.columns
+#
+#
+# def ffm_format(row):
+#     str_ = '1' if int(row[0]) == 1 else '-1'
+#     for i, v in enumerate(row[1:]):
+#         # get rid of zero values
+#         if v == 0:
+#             continue
+#         else:
+#             str_ += '\t%d:%d:%s' % (fields.index(features[i + 1].split('_')[0]) - 1, i, str(v))
+#     return str_
+#
+#
+# formatted = ffm_raw.apply(ffm_format, axis=1)
+#
+# formatted.to_csv('%sformatted_ffm_raw_test4.csv' % d, header=False, index=False)
