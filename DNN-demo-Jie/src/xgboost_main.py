5f0b114942e2f05315901f5f3d1968580bc3eb01
Jie <jie.he366@gmail.com>
2017-05-31 22:21:22 +0000

    u

--- DNN-demo-Jie/src/xgboost_main.py
+++ DNN-demo-Jie/src/xgboost_main.py
@@ -16,6 +16,9 @@ parser.add_argument('-et', type=str, default=None,
 parser.add_argument('-nm', action='store_true', 
     help='not mask future information')
 
+parser.add_argument('-nc', action='store_true', 
+    help='not use cached data')
+
 
 g = parser.add_mutually_exclusive_group(required=False)
 g.add_argument('-tdo', action='store_true',
@@ -107,63 +110,76 @@ features = ['advertiserID', 'sitesetID', 'age']
 #        'weekDay']
 features.reverse()
 
-tr_df = pd.read_csv('../data/pre/new_generated_train.csv')
-te_df_ = pd.read_csv('../data/pre/new_generated_test.csv')
-va_df = tr_df.loc[tr_df['clickTime_d'] == 24]
+if args.nc:
+    tr_df = pd.read_csv('../data/pre/new_generated_train.csv')
+    te_df_ = pd.read_csv('../data/pre/new_generated_test.csv')
+    va_df = tr_df.loc[tr_df['clickTime_d'] == 24]
 
-print(tr_df.head())
+    print(tr_df.head())
 
-tr_ui = pd.read_csv('../data/pre/new_tr_ui.csv', header=None).values
-te_ui = pd.read_csv('../data/pre/new_te_ui.csv', header=None).values
-va_ui = pd.read_csv('../data/pre/new_va_ui.csv', header=None).values
+    tr_ui = pd.read_csv('../data/pre/new_tr_ui.csv', header=None).values
+    te_ui = pd.read_csv('../data/pre/new_te_ui.csv', header=None).values
+    va_ui = pd.read_csv('../data/pre/new_va_ui.csv', header=None).values
 
-tr_ua = pd.read_csv('../data/pre/new_tr_ua.csv', header=None).values
-te_ua = pd.read_csv('../data/pre/new_te_ua.csv', header=None).values
-va_ua = pd.read_csv('../data/pre/new_va_ua.csv', header=None).values
-
-
-
-
-if args.rml:
-    print('--- Dropping day 30: ', len(tr_df[tr_df.clickTime_d == 30]))
-    tr_df = tr_df[tr_df.clickTime_d != 30]
-
-if args.tdo:
-    print('--- 17, 24 -> 31')
-    tr_df = tr_df.loc[(tr_df['clickTime_d'] == 17) | (tr_df['clickTime_d'] == 24)]
-
-if args.olv:
-    print('--- Using offline validation like: 17-23 -> 24 (val)')
-    tr_df = tr_df.loc[(tr_df['clickTime_d'] >= 17) & (tr_df['clickTime_d'] <= 23)]
-    if not args.nm:
-        print('--- Masked ', np.sum(tr_df.loc[tr_df['conversionTime_d'] 
-            >= 24, 'label'])/(np.sum(tr_df.loc[tr_df['clickTime_d'] == 23, 'label'])+1e-5))
-        tr_df.loc[tr_df['conversionTime_d'] >= 24, 'label'] = 0
+    tr_ua = pd.read_csv('../data/pre/new_tr_ua.csv', header=None).values
+    te_ua = pd.read_csv('../data/pre/new_te_ua.csv', header=None).values
+    va_ua = pd.read_csv('../data/pre/new_va_ua.csv', header=None).values
 
 
 
-x_all = pd.concat([tr_df[cate_features], te_df_[cate_features], va_df[cate_features]])
 
-encoded_x = None
-for f in cate_features:
-    feature = x_all[f].values
-    onehot_encoder = OneHotEncoder(sparse=False)
-    feature = onehot_encoder.fit_transform(feature)
-    if encoded_x is None:
-        encoded_x = feature
-    else:
-        encoded_x = np.concatenate((encoded_x, feature), axis=1)
-print("\nX shape: ", encoded_x.shape)
+    if args.rml:
+        print('--- Dropping day 30: ', len(tr_df[tr_df.clickTime_d == 30]))
+        tr_df = tr_df[tr_df.clickTime_d != 30]
+
+    if args.tdo:
+        print('--- 17, 24 -> 31')
+        tr_df = tr_df.loc[(tr_df['clickTime_d'] == 17) | (tr_df['clickTime_d'] == 24)]
+
+    if args.olv:
+        print('--- Using offline validation like: 17-23 -> 24 (val)')
+        tr_df = tr_df.loc[(tr_df['clickTime_d'] >= 17) & (tr_df['clickTime_d'] <= 23)]
+        if not args.nm:
+            print('--- Masked ', np.sum(tr_df.loc[tr_df['conversionTime_d'] 
+                >= 24, 'label'])/(np.sum(tr_df.loc[tr_df['clickTime_d'] == 23, 'label'])+1e-5))
+            tr_df.loc[tr_df['conversionTime_d'] >= 24, 'label'] = 0
 
 
-tr_x = np.concatenate((encoded_x[:,:len(tr_df)], tr_ui, tr_ua, tr_df[features].values), axis=1)
-te_x = np.concatenate((encoded_x[:,len(te_df_):-len(va_df)], te_ui, te_ua, te_df_[features].values), axis=1)
-te_x = np.concatenate((encoded_x[:,-len(va_df):], te_ui, te_ua, te_df_[features].values), axis=1)
-assert len(te_x)==len(te_df_)
 
-tr_y = tr_df['label'].values
-te_y = te_df_['label'].values
-va_y = va_df['label'].values
+    x_all = pd.concat([tr_df[cate_features], te_df_[cate_features], va_df[cate_features]])
+
+    encoded_x = None
+    for f in cate_features:
+        print('Onehot for  ', f)
+        feature = x_all[f].values.reshape(-1,1)
+        onehot_encoder = OneHotEncoder(sparse=False)
+        feature = onehot_encoder.fit_transform(feature)
+        if encoded_x is None:
+            encoded_x = feature
+        else:
+            encoded_x = np.concatenate((encoded_x, feature), axis=1)
+    print("\nX shape: ", encoded_x.shape)
+
+
+    tr_x = np.concatenate((encoded_x[:len(tr_df)], tr_ui, tr_ua, tr_df[features].values), axis=1)
+    te_x = np.concatenate((encoded_x[len(tr_df_):len(tr_df_)+len(te_df)], te_ui, te_ua, te_df_[features].values), axis=1)
+    va_x = np.concatenate((encoded_x[-len(va_df):], va_ui, va_ua, va_df_[features].values), axis=1)
+    assert len(te_x)==len(te_df_)
+
+    tr_y = tr_df['label'].values.reshape(-1,1)
+    te_y = te_df_['label'].values.reshape(-1,1)
+    va_y = va_df['label'].values.reshape(-1,1)
+
+
+import pickle
+try:
+    (tr_x,te_x,va_x,tr_y,te_y,va_y) = pickle.load( open('../data/pre/dump_xgboost.bin', 'rb'))
+    print('Cached data')
+except Exception as e:
+    print('Cached data not found')
+    pickle.dump((tr_x,te_x,va_x,tr_y,te_y,va_y), open('../data/pre/dump_xgboost.bin', 'wb'))
+
+
 
 # tr_df = tr_df[features+['label']]
 # va_df = va_df[features+['label']]
@@ -203,7 +219,7 @@ if 0:
 gbm = xgb.XGBClassifier(max_depth=5, max_delta_step=1, silent=True, n_estimators=500, 
                         learning_rate=0.3, objective='binary:logistic', 
                         min_child_weight = 1, scale_pos_weight = 1,  
-                        subsample=0.8, colsample_bytree=0.8,
+                        subsample=0.8, colsample_bytree=0.8, nthread=16,
                        
                        ).fit(tr_x, tr_y, eval_set=[(va_x, va_y)], 
                         eval_metric='logloss', verbose=True)
